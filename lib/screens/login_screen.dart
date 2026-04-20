73006b86ee43ce64f2bbdb73d3e263184873fa0e
Verubio <luis.7w7.angel@gmail.com>
2026-04-19 21:57:30 -0600

    Estructura de rutas y cascarones de interfaces listos

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class login extends StatelessWidget {
+  const login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Registro de Emociones')),
+      body: const Center(child: Text('Espacio para desarrollar la interfaz')),
+    );
+  }
+}
---
+++ lib/screens/registro_emo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class reg_emo extends StatelessWidget {
+  const reg_emo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Registro de Emociones')),
+      body: const Center(child: Text('Espacio para desarrollar la interfaz')),
+    );
+  }
+}
